040e40a8a8e3361a747be5bf7466ce4fdfa9a1f4
Victor <vn28007@gmail.com>
2020-05-16 09:13:59 -0300

    Okay, it works

--- insta.py
+++ insta.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from time import sleep
-
+from secrets import username, pw
 
 class InstaBot:
     def __init__(self, username, pw):
         self.driver = webdriver.Chrome()
+        self.username = username
         self.driver.get("https://instagram.com/")
         sleep(2)
         self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')\
@@ -14,8 +15,9 @@ class InstaBot:
         sleep(2)
         self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div')\
             .click()
-        sleep(2)
+        sleep(3)
         self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]')\
             .click()
+        sleep(2)
 
-my_bot = InstaBot('ctor.exe@icloud.com', '23568912')
+my_bot = InstaBot(username, pw)
